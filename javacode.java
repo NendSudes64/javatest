eea5af51e2b9c948dff9379772bd23f5b7ef511d
NendSudes64 <55093186+NendSudes64@users.noreply.github.com>
2024-03-15 10:34:05 +0100

    Create javacode.java

---
+++ javacode.java
@@ -0,0 +1,11 @@
+public class code {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        try {
+            Thread.sleep(5000); // Wait for 1 second
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+//ayo
