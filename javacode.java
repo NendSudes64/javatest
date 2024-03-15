55bd6d4c873e324a81b950d5e98469dbbbeece1b
Rasmus Østfeldt <rasmus@oestfeldt.net>
2024-03-15 10:39:26 +0100

    made som epiiic changes

--- javacode.java
+++ javacode.java
@@ -1,11 +1,13 @@
 public class javacode {
     public static void main(String[] args) {
+        while (true){
         System.out.println("Hello, World!");
         try {
             Thread.sleep(5000); // Wait for 1 second
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
     }
 }
 //ayo
